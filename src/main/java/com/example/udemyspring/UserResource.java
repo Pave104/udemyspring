6333e0e9e7da3958cf80d1c765cc8cf3675d85d9
pave1 <pave104@yahoo.com.br>
2022-07-23 21:02:37 -0300

    segundo commit

---
+++ src/main/java/com/example/udemyspring/UserResource.java
@@ -0,0 +1,21 @@
+package com.example.udemyspring;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+@GetMapping
+public ResponseEntity<User> findAll(){
+
+    User u=new User(1L,"John Kenedy","jon@email","9889-9877","senha");
+     return ResponseEntity.ok().body(u);
+
+
+}
+
+}
